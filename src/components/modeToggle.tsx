52aea96eafa0e890af7376b567363d585e3ed640
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-13 04:58:04 +0900

    モード切り替えボタンの外枠を削除した

--- src/components/modeToggle.tsx
+++ src/components/modeToggle.tsx
@@ -20,7 +20,7 @@ export function ModeToggleButton() {
 
     return (
         <div>
-            <Button onClick={ModeToggle} variant="outline" size="icon">
+            <Button onClick={ModeToggle} variant="noDesign" size="icon">
                 <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                 <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             </Button>
