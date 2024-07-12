322becb63648e98d84d38fbcf12f0b5042a73e9d
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-13 04:59:24 +0900

    タブレット以下で モード切り替えボタンが真ん中ではなく右寄せになるように変更した

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 import React from "react";
 import HeaderNav from "./HeaderNav";
 import { ModeToggleButton } from "./modeToggle";
+import SheetNav from "./SheetNav";
 
 const Header = () => {
     return (
         <header className="mx-auto flex w-[90%] items-center justify-between py-2">
-            <h1>
+            <h1 className="font-semibold">
                 <Link href="/">Site LOGO</Link>
             </h1>
             <HeaderNav></HeaderNav>
-            <ModeToggleButton></ModeToggleButton>
+            <div className="flex wrap gap-4">
+                <ModeToggleButton></ModeToggleButton>
+                <SheetNav></SheetNav>
+            </div>
         </header>
     );
 };
