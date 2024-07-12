24222abb49e45cd6fdc4599673955e6c8c9f53d9
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-13 02:08:35 +0900

    コンタクトページの追加

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionTitle from "../components/SectionTitle";
+
+const ContactPage = () => {
+    return (
+        <section>
+            <SectionTitle>CONTACT</SectionTitle>
+        </section>
+    );
+};
+
+export default ContactPage;
