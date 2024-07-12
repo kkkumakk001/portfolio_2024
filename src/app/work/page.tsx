b0086e5606ad2e7ebe5dffb49d2bd8e7464326ff
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-13 01:18:42 +0900

    workページの追加

---
+++ src/app/work/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkPage = () => {
+    return (
+        <section>
+            <h2 className="text-xl">sectionページ</h2>
+        </section>
+    );
+};
+
+export default WorkPage;
