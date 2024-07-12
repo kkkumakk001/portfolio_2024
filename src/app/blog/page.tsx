9ccee7a965fcd42bcd6c82a57119286fa98f919d
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-13 02:04:47 +0900

    blogページの追加

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionTitle from "../components/SectionTitle";
+
+const BlogPage = () => {
+    return (
+        <section>
+            <SectionTitle>BLOG</SectionTitle>
+        </section>
+    );
+};
+
+export default BlogPage;
