c19426b7d0c7584d04a40b789e37066684784b98
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-18 05:13:19 +0900

    Heroセクションの作成

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import SnsButtons from "./SnsButtons";
 import Container from "./Container";
+import Image from "next/image";
+import HeroImage from "../../public/images/HeroImage.png";
+import ContactButton from "./ContactButton";
 
 const Hero = () => {
     return (
         <section>
             <Container>
-                <SnsButtons></SnsButtons>
+                <div className="flex flex-col lg:flex-row wrap justify-center lg:gap-10">
+                    <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+                        <Image
+                            src={HeroImage}
+                            alt="メインの画像です"
+                            width={400}
+                            height={400}
+                        ></Image>
+                    </div>
+                    <div className="w-full lg:w-1/2 flex flex-col wrap items-center lg:items-start lg:justify-center">
+                        <p className="font-bold text-4xl mb-8">K.K's PortFolio</p>
+                        <SnsButtons></SnsButtons>
+                        <ContactButton></ContactButton>
+                    </div>
+                </div>
             </Container>
         </section>
     );
