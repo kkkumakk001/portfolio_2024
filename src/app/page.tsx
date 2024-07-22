382b45f30c89be35eaa472234e644c29b4830687
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-23 01:39:22 +0900

    マーキーコンポーネントの読み込み

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import SkillsMarquee from "@/components/Marquee";
 
 export default function Home() {
     return (
         <section>
             <Hero></Hero>
+            <SkillsMarquee></SkillsMarquee>
         </section>
     );
 }
