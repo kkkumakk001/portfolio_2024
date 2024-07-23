273940aea8908b875266bf83d6c719ac0c0489d7
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-24 01:36:15 +0900

    スキルセクションの作成
    アイテムの個数が中途半端な時、左寄せになるように工夫する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import SkillsMarquee from "@/components/Marquee";
+import SkillSection from "@/components/SkillSection";
 
 export default function Home() {
     return (
         <section>
             <Hero></Hero>
             <SkillsMarquee></SkillsMarquee>
+            <SkillSection></SkillSection>
         </section>
     );
 }
--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -1,5 +1,15 @@
-const SkillSection = () => {
-    return <section></section>;
-};
+import Container from "./Container";
+import SectionTitle from "./SectionTitle";
+import SkillCard from "./SkillCard";
 
+const SkillSection = () => {
+    return (
+        <Container>
+            <section>
+                <SectionTitle>Skills</SectionTitle>
+                <SkillCard></SkillCard>
+            </section>
+        </Container>
+    );
+};
 export default SkillSection;
