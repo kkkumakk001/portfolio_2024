e48b0a84835aa9dd8162145233b7fa7d7211a697
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-07-27 00:01:00 +0900

    スキルレベルを星の数で表すようにした。 後でレベル感を追記する。

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -10,43 +10,107 @@ import {
     SiTypescript,
     SiJavascript,
 } from "@icons-pack/react-simple-icons";
+import { StarIcon, StarFilledIcon } from "@radix-ui/react-icons";
 
 const skills = [
     {
         icon: <SiJavascript />,
         label: "JavaScript",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
     {
         icon: <SiTypescript />,
         label: "TypeScript",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
     {
         icon: <SiReact />,
         label: "React",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
     {
         icon: <SiNextdotjs />,
         label: "Next.js",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
     {
         icon: <SiTailwindcss />,
         label: "Tailwind CSS",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
     {
         icon: <SiShadcnui />,
         label: "Shadcn/ui",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
     {
         icon: <SiSupabase />,
         label: "Supabase",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
     {
         icon: <SiVercel />,
         label: "Vercel",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
     {
         icon: <SiGithub />,
         label: "GitHub",
+        level: (
+            <div className="flex flex-wrap justify-center">
+                <StarFilledIcon />
+                <StarFilledIcon />
+                <StarIcon />
+            </div>
+        ),
     },
 ];
 
@@ -54,19 +118,21 @@ const SkillCard = () => {
     return (
         <div className="flex flex-wrap w-full justify-around mt-8">
             {skills.map((skill) => (
-                <Card
-                    key={skill.label}
-                    className="min-w-[200px] max-w-[400px] w-[80%] sm:w-[45%] xl:w-[30%] mb-12 hover:bg-accent/90 duration-200"
-                >
-                    <CardContent className="flex justify-center items-center pt-6 pb-2">
+                // <Card
+                //     key={skill.label}
+                //     className="min-w-[200px] max-w-[400px] w-[80%] sm:w-[45%] xl:w-[30%] mb-12 hover:bg-accent/90 duration-200"
+                // >
+                <Card key={skill.label} className="w-[137px] mb-12 hover:bg-accent/90 duration-200">
+                    <CardContent className="flex justify-center items-center pt-4 pb-2">
                         {skill.icon}
                     </CardContent>
-                    <CardHeader className="pt-0">
-                        <CardTitle className="text-lg font-medium text-center">
+                    <CardHeader className="pt-0 pb-2">
+                        <CardTitle className="text-sm font-semibold text-center">
                             {skill.label}
                         </CardTitle>
-                        <CardDescription className="text-justify pt-2">
-                            各スキルの説明です、これはダミーです。テキストを入力してください。
+                        <CardDescription className="text-justify">
+                            {/* 各スキルの説明です、これはダミーです。テキストを入力してください。 */}
+                            {skill.level}
                         </CardDescription>
                     </CardHeader>
                 </Card>
