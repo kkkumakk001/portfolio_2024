e96da1fd867387a42fc90b698d7d0bd78c1887d4
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-06 06:57:09 +0900

    SkillLevelコンポーネントの読み込み

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -1,9 +1,11 @@
 import SectionWrap from "./SectionWrap";
 import SkillCard from "./SkillCard";
+import SkillLevel from "./SkillLevel";
 
 const SkillSection = () => {
     return (
         <SectionWrap title="Skill" desc="実務経験がないため、独自の基準でレベル感を表記しました。">
+            <SkillLevel></SkillLevel>
             <SkillCard></SkillCard>
         </SectionWrap>
     );
