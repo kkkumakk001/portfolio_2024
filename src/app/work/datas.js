2928671f09d600a06d6688c5c453d9c6d20d4a13
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-13 02:13:58 +0900

    作品のdataを別ファイルで管理

---
+++ src/app/work/datas.js
@@ -0,0 +1,28 @@
+const works = [
+    {
+        id: 1,
+        title: "ポートフォリオ",
+        link: "https://github.com/kkkumakk001",
+        date: "2024.8",
+        desc: "本サイトです。経歴、スキル、作品などを掲載しております。",
+        skills: ["Next.js", "TailwindCSS", "shadcn/ui", "TypeScript"],
+    },
+    {
+        id: 2,
+        title: "ブログ",
+        link: "https://github.com/kkkumakk001",
+        date: "2024.8",
+        desc: "microCMSでコンテンツを管理しAPIで記事を取得しています。",
+        skills: ["Next.js", "TailwindCSS", "shadcn/ui", "TypeScript"],
+    },
+    {
+        id: 3,
+        title: "ぽけくらべ",
+        link: "https://github.com/kkkumakk001",
+        date: "2024.8",
+        desc: "お気に入りのポケモンと背比べができます",
+        skills: ["Next.js", "TailwindCSS", "shadcn/ui", "TypeScript"],
+    },
+];
+
+export default works;
