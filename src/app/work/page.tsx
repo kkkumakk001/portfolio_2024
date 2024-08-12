eb799eed4618668a3ccd276f7c8d2cca7df1cf61
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-08-13 02:17:56 +0900

    WorkCardコンポーネントの役割変更によるpropsの追加と別ファイルに作品情報を分割した

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import SectionWrap from "../../components/SectionWrap";
+import WorkCard from "@/components/WorkCard";
+import works from "./datas";
 
 const WorkPage = () => {
     return (
         <SectionWrap
             title="WORK"
             desc="これまでに作成した作品です。コードや詳しい説明はREADMOREボタンから見れます。"
-        ></SectionWrap>
+        >
+            <WorkCard works={works}></WorkCard>
+        </SectionWrap>
     );
 };
 
