706b39cb7a70cc4b854d70b421f14b9582f1c8a8
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-11-03 08:54:36 +0900

    [style]: 余計なコードを削除

--- src/components/SnsButtons.tsx
+++ src/components/SnsButtons.tsx
@@ -3,26 +3,23 @@ import { Button } from "./ui/button";
 import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 // import Image from "next/image";
-// import QiitaIcon from "../../public/images/qiita_logo_icon.png";
 // import ZennIcon from "../../public/images/zenn_logo_icon.png";
 
 const SnsButtons = () => {
     return (
-        <div className="flex wrap gap-2 mb-8">
-            <Button variant="outline" size="icon" className="rounded-full">
+        <div className="flex wrap gap-2 mb-8 text-primary">
+            <Button variant="outline" size="icon" className="rounded-full hover:bg-background/90">
                 <Link href="https://x.com" target="_blank">
                     <TwitterLogoIcon className="w-4 h-4"></TwitterLogoIcon>
                 </Link>
             </Button>
-            <Button variant="outline" size="icon" className="rounded-full">
+            <Button variant="outline" size="icon" className="rounded-full hover:bg-background/90">
                 <Link href="https://github.com/kkkumakk001" target="_blank">
                     <GitHubLogoIcon></GitHubLogoIcon>
                 </Link>
             </Button>
+
             {/* <Button variant="outline" size="icon" className="rounded-full">
-                <Image src={QiitaIcon} alt="qiitaのアイコンです" width={16} height={16}></Image>
-            </Button>
-            <Button variant="outline" size="icon" className="rounded-full">
                 <Image
                     src="/zenn_logo_icon.png"
                     alt="zennのアイコンです"
