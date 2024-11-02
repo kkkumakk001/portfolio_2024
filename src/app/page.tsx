d2f5655bef5a60a321535eff7f84d12b1271846f
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-11-03 08:45:51 +0900

    [fixed]: トップページにはページ遷移アニメーションを適用しないようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import Hero from "@/components/Hero";
 import AboutSection from "@/components/AboutSection";
 import SkillsMarquee from "@/components/Marquee";
 import SkillSection from "@/components/SkillSection";
+import Template from "../components/template";
 
 export default function Home() {
     return (
-        <>
+        <Template disableAnimation>
             <Hero />
             <SkillsMarquee />
             <AboutSection />
             <SkillSection />
-        </>
+        </Template>
     );
 }
