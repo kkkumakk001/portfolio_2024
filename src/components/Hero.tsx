1d9a97a5ad06c80e97a0b05b6ef010fe549c7f0f
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-11-03 08:47:57 +0900

    [add]: タイプライターの追加、背景デザインの追加

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,32 +1,31 @@
 import React from "react";
 import SnsButtons from "./SnsButtons";
-import Container from "./Container";
-import Image from "next/image";
-import HeroImage from "../../public/images/HeroImage.png";
 import ContactButton from "./ContactButton";
+import ScrollDown from "./ScrollDown";
+import TypeWriter from "./Typewriter";
 
 const Hero = () => {
     return (
-        <section>
-            <Container>
-                <div className="flex flex-col lg:flex-row wrap justify-center lg:gap-10">
-                    <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
-                        <Image
-                            src={HeroImage}
-                            alt="メインの画像です"
-                            width={400}
-                            height={400}
-                            priority
-                        ></Image>
-                    </div>
-                    <div className="w-full lg:w-1/2 flex flex-col wrap items-center lg:items-start lg:justify-center">
-                        <p className="font-bold text-4xl mb-8 tracking-wider">{`K.K's PortFolio`}</p>
-                        <SnsButtons></SnsButtons>
-                        <ContactButton></ContactButton>
-                    </div>
-                </div>
-            </Container>
-        </section>
+        <div className="relative font-en h-dvh flex items-center justify-center overflow-hidden">
+            <div className="absolute inset-0 opacity-20">
+                <div className="absolute top-1/4 left-1/4 w-1/2 h-1/2 border border-blue-400 transform rotate-45"></div>
+                <div className="absolute top-1/4 left-1/4 w-1/2 h-1/2 border border-blue-400 transform -rotate-45"></div>
+            </div>
+
+            <div className="relative z-10 text-center px-4 max-w-3xl">
+                <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6 tracking-tight">
+                    <span className="text-blue-400">
+                        <span className="inline-block h-[49px] md:h-[65px] lg:h-[82px]"></span>
+                        <TypeWriter />
+                    </span>
+                </h1>
+                <span className="flex justify-center">
+                    <SnsButtons />
+                </span>
+                <ContactButton />
+            </div>
+            <ScrollDown />
+        </div>
     );
 };
 
