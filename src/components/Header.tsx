370b2e8379506ba033cb327bc0297c4b25eea39f
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2024-11-11 09:28:50 +0900

    [style]: ヘッダーの背景色を透明からページ全体と同じ色に変更した

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import SheetNav from "./SheetNav";
 
 const Header = () => {
     return (
-        <header className="fixed font-en z-50 w-full py-2 bg-transparent text-white">
+        <header className="fixed font-en z-50 w-full py-2 bg-gray-900 text-white">
             <nav className="mx-auto w-[95%] flex justify-between">
                 <div className="flex items-center">
                     <h1 className="text-lg mr-4">
